6c0ab2ec8f1a3a6e6191a68f0a5ccb02610feaa1
ElBerros <0283257@up.edu.mx>
2025-11-06 20:45:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "El Berros Rules"
+    return "Hello World!"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
